49d16afee38f49b7f3186204b0d59e9c61f11cf0
keremyilmaz <keremyilmaz499@gmail.com>
2024-03-08 08:21:49 +0000

    :books: Security Configuration files' reformat and beautified documentation.

---
+++ src/main/java/io/passport/server/config/KeycloakProvider.java
@@ -0,0 +1,44 @@
+package io.passport.server.config;
+
+import lombok.Getter;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Class which allows the Keycloak methods to be called on Spring.
+ * //Additional methods for Keycloak can be implemented here if needed.
+ */
+@Configuration
+@Getter
+public class KeycloakProvider {
+
+    /**
+     * Keycloak variables that are necessary to access the desired Keycloak Realm and Client.
+     * Values are provided in resources/application.properties
+     */
+    @Value("${keycloak.auth-server-url}")
+    public String serverURL;
+    @Value("${keycloak.realm}")
+    public String realm;
+    @Value("${keycloak.resource}")
+    public String clientID;
+    @Value("${keycloak.credentials.secret}")
+    public String clientSecret;
+
+    /**
+     * @param username user Keycloak recorded username
+     * @param password user Keycloak recorded password
+     * @return The main Keycloak method we need for now
+     * Called to create an instance of the Keycloak in Spring, in our controllers
+     */
+    public KeycloakBuilder newKeycloakBuilderWithPasswordCredentials(String username, String password) {
+        return KeycloakBuilder.builder() //
+                .realm(realm) //
+                .serverUrl(serverURL)//
+                .clientId(clientID) //
+                .clientSecret(clientSecret) //
+                .username(username) //
+                .password(password);
+    }
+}
