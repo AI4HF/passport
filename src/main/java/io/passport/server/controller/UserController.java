2418d4a200d3b86a0b244694e9a6547631df507c
keremyilmaz <keremyilmaz499@gmail.com>
2024-03-12 13:38:18 +0000

    :sparkles: Login request has been changed to serve the token to the client, instead of the list of roles. KcBuilder related changes are not included in this commit.

--- src/main/java/io/passport/server/controller/UserController.java
+++ src/main/java/io/passport/server/controller/UserController.java
@@ -1,7 +1,5 @@
 package io.passport.server.controller;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import io.passport.server.config.KeycloakProvider;
 import io.passport.server.model.LoginRequest;
 import org.keycloak.admin.client.Keycloak;
@@ -16,10 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.BadRequestException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Spring Controller which implements the Authentication measures and role management
@@ -35,34 +29,22 @@ public class UserController {
     public UserController(KeycloakProvider kcProvider) {
         this.kcProvider = kcProvider;
     }
-
     /**
      * Our main login method
      * Handles the Keycloak Authentication process, access token and resource_access retrieval.
      * Extracts the user's roles on the client and returns them in the body to allow the Frontend work based around it.
      */
     @PostMapping("/login")
-    public ResponseEntity<List<String>> login(@NotNull @RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<String> login(@NotNull @RequestBody LoginRequest loginRequest) {
         Keycloak keycloak = kcProvider.newKeycloakBuilderWithPasswordCredentials(loginRequest.getUsername(), loginRequest.getPassword()).build();
-
         AccessTokenResponse accessTokenResponse = null;
         try {
             accessTokenResponse = keycloak.tokenManager().getAccessToken();
             String accessToken = accessTokenResponse.getToken();
-            DecodedJWT decodedJWT = JWT.decode(accessToken);
-
-            Map<String, Object> resourceAccess = decodedJWT.getClaim("resource_access").asMap();
-            Map<String, Object> clientRoles = (Map<String, Object>) resourceAccess.get(kcProvider.clientID);
-
-            List<String> roles = new ArrayList<>();
-            if (clientRoles != null && clientRoles.containsKey("roles")) {
-                roles.addAll((List<String>) clientRoles.get("roles"));
-            }
-
-            return ResponseEntity.status(HttpStatus.OK).body(roles);
+            return ResponseEntity.status(HttpStatus.OK).body(accessToken);
         } catch (BadRequestException ex) {
             LOG.warn("invalid account creds.", ex);
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Collections.emptyList());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("");
         }
     }
 }
