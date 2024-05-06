e49f0402edc54a7fb23e6ff94c2123669251b798
keremyilmaz <keremyilmaz499@gmail.com>
2024-05-06 12:39:44 +0100

    :memo: Useless methods are deleted, documentations are updated and some unused imports are removed.

--- src/main/java/io/passport/server/repository/PersonnelRepository.java
+++ src/main/java/io/passport/server/repository/PersonnelRepository.java
@@ -3,15 +3,9 @@ package io.passport.server.repository;
 import io.passport.server.model.Personnel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 /**
  * Personnel repository for database management.
  */
 @Repository
-public interface PersonnelRepository extends JpaRepository<Personnel, Long> {
-
-    Optional<Personnel> findByPersonId(String personnelId);
-
-
-}
+public interface PersonnelRepository extends JpaRepository<Personnel, Long> { }
--- src/main/java/io/passport/server/repository/StudyRepository.java
+++ src/main/java/io/passport/server/repository/StudyRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 /**
  * Study repository for database management.
@@ -12,12 +11,8 @@ import java.util.Optional;
 @Repository
 public interface StudyRepository extends JpaRepository<Study, Long> {
     /**
-     * Study id search method in case unique id is decided to be not enough.
-     * @param studyId Non-unique study identifier set by users.
-     * @return
+     * Repository generated method for finding all the studies with the given page parameters.
+     * @param page Pageable parameter which sets the required amount of data from the database.
      */
-    Optional<Study> findByStudyId(String studyId);
     Page<Study> findAll(Pageable page);
-
-
 }
