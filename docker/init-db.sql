7289e93d8114eee19f9dbf62098393c3d04f3928
keremyilmaz <keremyilmaz499@gmail.com>
2024-05-06 12:38:09 +0100

    :bug: Some general table elements are renamed for easier understanding and a certain bug related to poor reference key assignment is fixed.

--- docker/init-db.sql
+++ docker/init-db.sql
@@ -1,20 +1,18 @@
 -- Create organizations table
 CREATE TABLE organizations (
-                               id SERIAL PRIMARY KEY,
-                               organization_id VARCHAR(255) NOT NULL,
+                               organization_id SERIAL PRIMARY KEY,
                                name VARCHAR(255),
                                address VARCHAR(255)
 );
 
 -- Insert dummy organizations
-INSERT INTO organizations (organization_id, name, address) VALUES
-                                                               ('ai4hf_aumc', 'Amsterdam UMC', 'Address of Amsterdam UMC');
+INSERT INTO organizations (name, address) VALUES
+                                                               ('Amsterdam UMC', 'Address of Amsterdam UMC');
 
 -- Create personnel table
 CREATE TABLE personnel (
-                           id SERIAL PRIMARY KEY,
-                           person_id VARCHAR(255) NOT NULL,
-                           organization_id VARCHAR(255) REFERENCES organizations(organization_id),
+                           person_id SERIAL PRIMARY KEY,
+                           organization_id SERIAL REFERENCES organizations(organization_id),
                            first_name VARCHAR(255),
                            last_name VARCHAR(255),
                            role VARCHAR(255),
@@ -22,20 +20,19 @@ CREATE TABLE personnel (
 );
 
 -- Insert dummy personnel
-INSERT INTO personnel (person_id, organization_id, first_name, last_name, role, email) VALUES
-                                                                                           ('person1', 'ai4hf_aumc', 'John', 'Doe', 'Data Scientist', 'John.doe@emailhost.com');
+INSERT INTO personnel (organization_id, first_name, last_name, role, email) VALUES
+                                                                                           (1, 'John', 'Doe', 'Data Scientist', 'John.doe@emailhost.com');
 
 -- Create studies table
 CREATE TABLE studies (
-                         id SERIAL PRIMARY KEY,
-                         study_id VARCHAR(255) NOT NULL,
+                         study_id SERIAL PRIMARY KEY,
                          name VARCHAR(255),
                          description TEXT,
                          objectives TEXT,
                          ethics TEXT,
-                         owner_id VARCHAR(255) REFERENCES personnel(person_id)
+                         owner_id SERIAL REFERENCES personnel(person_id)
 );
 
 -- Insert dummy studies
-INSERT INTO studies (study_id, name, description, objectives, ethics, owner_id) VALUES
-                                                                                    ('study1', 'Risk score for acute HF in the emergency department', 'Predicting risk factors for acute HF…', 'Evaluating the risk prediction for acute HF', 'Approved by Ethical Board on 2023-01-15, Application Number: 123', 'person1');
+INSERT INTO studies (name, description, objectives, ethics, owner_id) VALUES
+                                                                                    ('Risk score for acute HF in the emergency department', 'Predicting risk factors for acute HF…', 'Evaluating the risk prediction for acute HF', 'Approved by Ethical Board on 2023-01-15, Application Number: 123', 1);
