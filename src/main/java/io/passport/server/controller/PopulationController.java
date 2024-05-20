18534754647e4dc3c0617de824c3b9cee380481b
keremyilmaz <keremyilmaz499@gmail.com>
2024-05-20 07:22:14 +0100

    :sparkles: Phase-one controllers implemented.

---
+++ src/main/java/io/passport/server/controller/PopulationController.java
@@ -0,0 +1,74 @@
+package io.passport.server.controller;
+
+import io.passport.server.model.Population;
+import io.passport.server.repository.PopulationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Class which stores the generated HTTP requests related to population operations.
+ */
+@RestController
+@RequestMapping("/populations")
+public class PopulationController {
+    /**
+     * Population repo access for database management.
+     */
+    private final PopulationRepository populationRepository;
+
+    @Autowired
+    public PopulationController(PopulationRepository populationRepository) {
+        this.populationRepository = populationRepository;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Population>> getAllPopulations(
+            @RequestParam Long studyId,
+            @RequestParam(defaultValue = "0") int page) {
+
+        Pageable pageable = PageRequest.of(page, 10);
+        Page<Population> populationPage = populationRepository.findByStudyId(studyId, pageable);
+
+        List<Population> populationList = populationPage.getContent();
+        long totalCount = populationPage.getTotalElements();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Total-Count", String.valueOf(totalCount));
+
+        return ResponseEntity.ok().headers(headers).body(populationList);
+    }
+
+    /**
+     * Create Population.
+     * @param population Population model instance to be created.
+     * @return
+     */
+    @PostMapping("/")
+    public ResponseEntity<Population> createPopulation(@RequestBody Population population) {
+        Population savedPopulation = populationRepository.save(population);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedPopulation);
+    }
+
+    /**
+     * Delete by Population ID.
+     * @param populationId ID of the population that is to be deleted.
+     * @return
+     */
+    @DeleteMapping("/{populationId}")
+    public ResponseEntity<Object> deletePopulation(@PathVariable Long populationId) {
+        return populationRepository.findById(populationId)
+                .map(population -> {
+                    populationRepository.delete(population);
+                    return ResponseEntity.noContent().build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
---
+++ src/main/java/io/passport/server/controller/SurveyController.java
@@ -0,0 +1,74 @@
+package io.passport.server.controller;
+
+import io.passport.server.model.Survey;
+import io.passport.server.repository.SurveyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Class which stores the generated HTTP requests related to survey operations.
+ */
+@RestController
+@RequestMapping("/surveys")
+public class SurveyController {
+    /**
+     * Survey repo access for database management.
+     */
+    private final SurveyRepository surveyRepository;
+
+    @Autowired
+    public SurveyController(SurveyRepository surveyRepository) {
+        this.surveyRepository = surveyRepository;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Survey>> getAllSurveys(
+            @RequestParam Long studyId,
+            @RequestParam(defaultValue = "0") int page) {
+
+        Pageable pageable = PageRequest.of(page, 10);
+        Page<Survey> surveyPage = surveyRepository.findByStudyId(studyId, pageable);
+
+        List<Survey> surveyList = surveyPage.getContent();
+        long totalCount = surveyPage.getTotalElements();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Total-Count", String.valueOf(totalCount));
+
+        return ResponseEntity.ok().headers(headers).body(surveyList);
+    }
+
+    /**
+     * Create Survey.
+     * @param survey Survey model instance to be created.
+     * @return
+     */
+    @PostMapping("/")
+    public ResponseEntity<Survey> createSurvey(@RequestBody Survey survey) {
+        Survey savedSurvey = surveyRepository.save(survey);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedSurvey);
+    }
+
+    /**
+     * Delete by Survey ID.
+     * @param surveyId ID of the survey that is to be deleted.
+     * @return
+     */
+    @DeleteMapping("/{surveyId}")
+    public ResponseEntity<Object> deleteSurvey(@PathVariable Long surveyId) {
+        return surveyRepository.findById(surveyId)
+                .map(survey -> {
+                    surveyRepository.delete(survey);
+                    return ResponseEntity.noContent().build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
