1fc460834e9757958eb3b4ee5c864f54d55e05cb
keremyilmaz <keremyilmaz499@gmail.com>
2024-07-08 08:01:00 +0100

    :memo: Credentials' documentation is fixed.

--- src/main/java/io/passport/server/model/Credentials.java
+++ src/main/java/io/passport/server/model/Credentials.java
@@ -1,9 +1,9 @@
 package io.passport.server.model;
 
+/**
+ * Credentials pair to pass the values in to the requests.
+ */
 public class Credentials {
-    /**
-     * Credentials pair to pass the values in to the requests.
-     */
     public String username;
     public String password;
 }
