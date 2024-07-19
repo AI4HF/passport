149ba99d7d7151c074ce420b63e0b66b704ebbe4
okanmercan <okanmercan16@gmail.com>
2024-07-19 09:54:32 +0300

    :sparkles: feat(ModelController): Implement model controller and related classes.

---
+++ src/main/java/io/passport/server/repository/ModelRepository.java
@@ -0,0 +1,13 @@
+package io.passport.server.repository;
+
+import io.passport.server.model.Model;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Model repository for database management.
+ */
+public interface ModelRepository extends JpaRepository<Model, Long> {
+    List<Model> findByStudyId(Long studyId);
+}
