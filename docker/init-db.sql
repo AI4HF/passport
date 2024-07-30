58931b834b5b14cbd5d3e17e4cb6873a47fe3e87
keremyilmaz <keremyilmaz499@gmail.com>
2024-07-30 08:38:34 +0100

    :bug: Personnel link initializations are fixed to fit the new personnel id data type.

--- docker/init-db.sql
+++ docker/init-db.sql
@@ -115,7 +115,7 @@ CREATE TABLE featureset (
 
 -- Insert dummy FeatureSet
 INSERT INTO featureset (experiment_id, title, featureset_url, description, created_at, created_by, last_updated_at, last_updated_by) VALUES
-    (1, 'Feature set for AI4HFsubstudy 2 – Risk score prediction for acute HF in the emergency department.', 'https://datatools4heart.eu/feature-sets/study1-features', 'Feature set containing feature information used in risk score prediction for acute HF in the emergency department.', '2023-01-01 00:00:00', 1, '2023-01-01 00:00:00', 1);
+    (1, 'Feature set for AI4HFsubstudy 2 – Risk score prediction for acute HF in the emergency department.', 'https://datatools4heart.eu/feature-sets/study1-features', 'Feature set containing feature information used in risk score prediction for acute HF in the emergency department.', '2023-01-01 00:00:00', 'service-account-admin', '2023-01-01 00:00:00', 'service-account-admin');
 
 -- Create Feature table
 CREATE TABLE feature (
@@ -138,7 +138,7 @@ CREATE TABLE feature (
 
 -- Insert dummy Feature
 INSERT INTO feature (featureset_id, title, description, data_type, feature_type, mandatory, isUnique, units, equipment, data_collection, created_at, created_by, last_updated_at, last_updated_by) VALUES
-    (1, 'age', 'Age of the patient at reference point (at the time of admission)', 'integer', 'numerical', true, false, 'years', '', 'Automatic Collection from Government Database', '2023-01-01 00:00:00', 1, '2023-01-01 00:00:00', 1);
+    (1, 'age', 'Age of the patient at reference point (at the time of admission)', 'integer', 'numerical', true, false, 'years', '', 'Automatic Collection from Government Database', '2023-01-01 00:00:00', 'service-account-admin', '2023-01-01 00:00:00', 'service-account-admin');
 
 -- Create Dataset table
 CREATE TABLE dataset (
@@ -160,7 +160,7 @@ CREATE TABLE dataset (
 
 -- Insert dummy Dataset
 INSERT INTO dataset (featureset_id, population_id, organization_id, title, description, version, reference_entity, num_of_records, synthetic, created_at, created_by, last_updated_at, last_updated_by) VALUES
-    (1, 1, 1, 'HF Risk Dataset', 'Dataset for HF Risk Prediction factors', '0.1', 'Encounter', 1562, false, '2023-01-01 00:00:00', 1, '2023-01-01 00:00:00', 1);
+    (1, 1, 1, 'HF Risk Dataset', 'Dataset for HF Risk Prediction factors', '0.1', 'Encounter', 1562, false, '2023-01-01 00:00:00', 'service-account-admin', '2023-01-01 00:00:00', 'service-account-admin');
 
 -- Create DatasetTransformation table
 CREATE TABLE dataset_transformation (
@@ -189,7 +189,7 @@ CREATE TABLE dataset_transformation_step (
 
 -- Insert dummy DatasetTransformationStep
 INSERT INTO dataset_transformation_step (data_transformation_id, input_features, output_features, method, explanation, created_at, created_by, last_updated_at, last_updated_by) VALUES
-    (1, 'feature1', 'feature1_1', 'Normalization', 'Decimal values are normalized between 0 and 1.', '2023-01-01 00:00:00', 1, '2023-01-01 00:00:00', 1);
+    (1, 'feature1', 'feature1_1', 'Normalization', 'Decimal values are normalized between 0 and 1.', '2023-01-01 00:00:00', 'service-account-admin', '2023-01-01 00:00:00', 'service-account-admin');
 
 -- Create LearningDataset table
 CREATE TABLE learning_dataset (
