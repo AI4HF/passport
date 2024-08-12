6b2ceecb3e205dbb18c45627d4df6fc784fe9601
okanmercan <okanmercan16@gmail.com>
2024-08-12 10:40:05 +0300

    :passport_control: feat(StudyPersonnelController): Implement role based authorization for StudyPersonnelController.

--- src/main/java/io/passport/server/controller/StudyPersonnelController.java
+++ src/main/java/io/passport/server/controller/StudyPersonnelController.java
@@ -1,12 +1,16 @@
 package io.passport.server.controller;
 
 import io.passport.server.model.Personnel;
+import io.passport.server.model.Role;
+import io.passport.server.service.RoleCheckerService;
 import io.passport.server.service.StudyPersonnelService;
+import org.keycloak.KeycloakPrincipal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,20 +29,37 @@ public class StudyPersonnelController {
      */
     private final StudyPersonnelService studyPersonnelService;
 
+    /**
+     * Role checker service for authorization
+     */
+    private final RoleCheckerService roleCheckerService;
+
     @Autowired
-    public StudyPersonnelController(StudyPersonnelService studyPersonnelService) {
+    public StudyPersonnelController(StudyPersonnelService studyPersonnelService, RoleCheckerService roleCheckerService) {
         this.studyPersonnelService = studyPersonnelService;
+        this.roleCheckerService = roleCheckerService;
     }
 
     /**
      * Get all personnel related to a study and an organization.
      * @param studyId ID of the study.
      * @param organizationId ID of the organization
+     * @param principal KeycloakPrincipal object that holds access token
      * @return
      */
     @GetMapping("/personnel")
-    public ResponseEntity<?> getPersonnelByStudyId(@RequestParam Long studyId, @RequestParam Long organizationId) {
+    public ResponseEntity<?> getPersonnelByStudyId(@RequestParam Long studyId,
+                                                   @RequestParam Long organizationId,
+                                                   @AuthenticationPrincipal KeycloakPrincipal<?> principal) {
         try{
+
+            // Allowed roles for this endpoint
+            List<Role> allowedRoles = List.of(Role.STUDY_OWNER);
+            // Check role of the user
+            if(!this.roleCheckerService.hasAnyRole(principal, allowedRoles)){
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            }
+
             List<Personnel> personnel = this.studyPersonnelService.findPersonnelByStudyIdAndOrganizationId(studyId, organizationId);
             return ResponseEntity.ok(personnel);
         }catch(Exception e){
@@ -52,11 +73,23 @@ public class StudyPersonnelController {
      * @param studyId ID of the study.
      * @param organizationId ID of the organization.
      * @param personnel List of personnel to be used in StudyPersonnel entries
+     * @param principal KeycloakPrincipal object that holds access token
      * @return
      */
     @PostMapping("/personnel")
-    public ResponseEntity<?> createStudyPersonnelEntries(@RequestParam Long studyId, @RequestParam Long organizationId, @RequestBody List<Personnel> personnel) {
+    public ResponseEntity<?> createStudyPersonnelEntries(@RequestParam Long studyId,
+                                                         @RequestParam Long organizationId,
+                                                         @RequestBody List<Personnel> personnel,
+                                                         @AuthenticationPrincipal KeycloakPrincipal<?> principal) {
         try{
+
+            // Allowed roles for this endpoint
+            List<Role> allowedRoles = List.of(Role.STUDY_OWNER);
+            // Check role of the user
+            if(!this.roleCheckerService.hasAnyRole(principal, allowedRoles)){
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            }
+
             this.studyPersonnelService.createStudyPersonnelEntries(studyId, organizationId, personnel);
             List<Personnel> updatedPersonnel = this.studyPersonnelService.findPersonnelByStudyIdAndOrganizationId(studyId, organizationId);
             return ResponseEntity.ok(updatedPersonnel);
