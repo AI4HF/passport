075b095e7c2232ac3fb555411c980bbbde8f4324
keremyilmaz <keremyilmaz499@gmail.com>
2024-09-16 08:23:19 +0100

    :art: Redundant import is removed.

--- src/main/java/io/passport/server/model/JsonConverter.java
+++ src/main/java/io/passport/server/model/JsonConverter.java
@@ -1,6 +1,5 @@
 package io.passport.server.model;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
