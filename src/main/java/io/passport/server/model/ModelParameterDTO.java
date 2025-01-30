c6ea96df888957b0fb2df11589173490b4352df4
keremyilmaz <keremyilmaz499@gmail.com>
2025-01-30 08:38:56 +0000

    :sparkles: Model Parameter Page utilities implemented.

---
+++ src/main/java/io/passport/server/model/ModelParameterDTO.java
@@ -0,0 +1,32 @@
+package io.passport.server.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Data Transfer Object for ModelParameter.
+ */
+@Getter
+@Setter
+public class ModelParameterDTO {
+    private Long modelId;
+    private Long parameterId;
+    private String type;
+    private String value;
+
+    /**
+     * Constructs a new ModelParameterDTO from a ModelParameter entity.
+     * @param entity the ModelParameter entity
+     */
+    public ModelParameterDTO(ModelParameter entity) {
+        this.modelId = entity.getId().getModelId();
+        this.parameterId = entity.getId().getParameterId();
+        this.type = entity.getType();
+        this.value = entity.getValue();
+    }
+
+    /**
+     * Default constructor created for implicit parameter initialization.
+     */
+    public ModelParameterDTO() {}
+}
