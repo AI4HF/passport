de94f18ca6b8cab06b0563fad6488331b24983da
keremyilmaz <keremyilmaz499@gmail.com>
2025-11-10 08:51:20 +0300

    :sparkles: Static Article integration into passport

--- src/main/java/io/passport/server/model/PassportDetails.java
+++ src/main/java/io/passport/server/model/PassportDetails.java
@@ -32,6 +32,8 @@ public class PassportDetails {
 
     private boolean experimentDetails;
 
+    private boolean staticArticleDetails;
+
     private boolean surveyDetails;
 
     private boolean studyDetails;
--- src/main/java/io/passport/server/service/PassportService.java
+++ src/main/java/io/passport/server/service/PassportService.java
@@ -49,6 +49,9 @@ public class PassportService {
     private ExperimentService experimentService;
 
     @Autowired
+    private StaticArticleService staticArticleService;
+
+    @Autowired
     private FeatureSetService featureSetService;
 
     @Autowired
@@ -136,6 +139,9 @@ public class PassportService {
             }
             if(passportWithDetailSelection.getPassportDetailsSelection().isExperimentDetails()){
                 detailsJson.put("experiments", fetchExperiments(passportWithDetailSelection.getPassport()));
+            }
+            if(passportWithDetailSelection.getPassportDetailsSelection().isStaticArticleDetails()){
+                detailsJson.put("staticArticles", fetchStaticArticles(passportWithDetailSelection.getPassport()));
             }
             if(passportWithDetailSelection.getPassportDetailsSelection().isFeatureSets()){
                 detailsJson.put("featureSetsWithFeatures", fetchFeatureSetsWithFeatures(passportWithDetailSelection.getPassport()));
@@ -249,6 +255,13 @@ public class PassportService {
             return experimentService.findExperimentByStudyId(passport.getStudyId());
         } catch (RuntimeException e) {
             throw new RuntimeException("Error fetching Experiments: " + e.getMessage());
+        }
+    }
+    private List<StaticArticle> fetchStaticArticles(Passport passport) {
+        try {
+            return staticArticleService.findByStudyId(passport.getStudyId());
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Error fetching Static Articles: " + e.getMessage());
         }
     }
 
